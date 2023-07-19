05f7f566f77321cdc991e0ffe40424c07de8c75c
Minh Tạ <taminh596@gmail.com>
2023-07-19 21:52:03 +0700

    update model

--- app.py
+++ app.py
@@ -16,21 +16,27 @@ imgSize = 300
 
 folder = "Data/C"
 counter = 0
-            # A        B        C           D       E           G       I
+# A        B        C           D       E           G       I
 labels = ["Nam dam", "4 ngon", "Chu C", "5 ngon", "2 ngon", "1 ngon", "3 ngon"]
 
 
-
-class handTracker():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, modelComplexity=1, trackCon=0.5):
+class handTracker:
+    def __init__(
+        self, mode=False, maxHands=2, detectionCon=0.5, modelComplexity=1, trackCon=0.5
+    ):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.modelComplex = modelComplexity
         self.trackCon = trackCon
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex,
-                                        self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(
+            self.mode,
+            self.maxHands,
+            self.modelComplex,
+            self.detectionCon,
+            self.trackCon,
+        )
         self.mpDraw = mp.solutions.drawing_utils
 
     def handsFinder(self, image, draw=True):
@@ -39,22 +45,21 @@ class handTracker():
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
-
                 if draw:
                     self.mpDraw.draw_landmarks(
-                        image, handLms, self.mpHands.HAND_CONNECTIONS)
+                        image, handLms, self.mpHands.HAND_CONNECTIONS
+                    )
         return image
 
-    def positionFinder(self, image, handNo=0, draw=True):
+    def positionFinder(self, image, handNo=0, draw=False):
         lmlist = []
         if self.results.multi_hand_landmarks:
             Hand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(Hand.landmark):
                 h, w, c = image.shape
-                cx, cy = int(lm.x*w), int(lm.y*h)
+                cx, cy = int(lm.x * w), int(lm.y * h)
                 lmlist.append([id, cx, cy])
-                
-                
+
             if draw:
                 cv2.circle(image, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
 
@@ -62,7 +67,6 @@ class handTracker():
 
 
 def main():
-
     cap = cv2.VideoCapture(0)
     tracker = handTracker()
 
@@ -74,16 +78,16 @@ def main():
             print(lmList[4])
         cv2.imshow("Video", image)
         cv2.waitKey(1)
-        
+
         success, img = cap.read()
         imgOutput = img.copy()
         hands, img = detector.findHands(img)
         if hands:
             hand = hands[0]
-            x, y, w, h = hand['bbox']
+            x, y, w, h = hand["bbox"]
 
             imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
-            imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+            imgCrop = img[y - offset : y + h + offset, x - offset : x + w + offset]
             aspectRatio = h / w
 
             if aspectRatio > 1:
@@ -91,7 +95,7 @@ def main():
                 wCal = math.ceil(k * w)
                 imgResize = cv2.resize(imgCrop, (wCal, imgSize))
                 wGap = math.ceil((imgSize - wCal) / 2)
-                imgWhite[:, wGap:wCal + wGap] = imgResize
+                imgWhite[:, wGap : wCal + wGap] = imgResize
                 prediction, index = classifier.getPrediction(imgWhite, draw=False)
 
             else:
@@ -99,15 +103,27 @@ def main():
                 hCal = math.ceil(k * h)
                 imgResize = cv2.resize(imgCrop, (imgSize, hCal))
                 hGap = math.ceil((imgSize - hCal) / 2)
-                imgWhite[hGap:hCal + hGap, :] = imgResize
+                imgWhite[hGap : hCal + hGap, :] = imgResize
                 prediction, index = classifier.getPrediction(imgWhite, draw=False)
 
-            cv2.putText(imgOutput, labels[index], (x, y -26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
-            cv2.rectangle(imgOutput, (x-offset, y-offset),
-                        (x + w+offset, y + h+offset), (255, 0, 255), 4)
+            cv2.putText(
+                imgOutput,
+                labels[index],
+                (x, y - 26),
+                cv2.FONT_HERSHEY_COMPLEX,
+                1.7,
+                (255, 255, 255),
+                2,
+            )
+            cv2.rectangle(
+                imgOutput,
+                (x - offset, y - offset),
+                (x + w + offset, y + h + offset),
+                (255, 0, 255),
+                4,
+            )
             cv2.imshow("Image", imgOutput)
             cv2.waitKey(1)
-      
 
 
 if __name__ == "__main__":
--- dataCollection.py
+++ dataCollection.py
@@ -10,7 +10,7 @@ detector = HandDetector(maxHands=1)
 offset = 20
 imgSize = 300
 
-folder = "Data/E"
+folder = "Data/I"
 counter = 0
 
 while True:
@@ -18,10 +18,10 @@ while True:
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
-        x, y, w, h = hand['bbox']
+        x, y, w, h = hand["bbox"]
 
         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
-        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+        imgCrop = img[y - offset : y + h + offset, x - offset : x + w + offset]
 
         imgCropShape = imgCrop.shape
 
@@ -33,7 +33,7 @@ while True:
             imgResize = cv2.resize(imgCrop, (wCal, imgSize))
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize - wCal) / 2)
-            imgWhite[:, wGap:wCal + wGap] = imgResize
+            imgWhite[:, wGap : wCal + wGap] = imgResize
 
         else:
             k = imgSize / w
@@ -41,7 +41,7 @@ while True:
             imgResize = cv2.resize(imgCrop, (imgSize, hCal))
             imgResizeShape = imgResize.shape
             hGap = math.ceil((imgSize - hCal) / 2)
-            imgWhite[hGap:hCal + hGap, :] = imgResize
+            imgWhite[hGap : hCal + hGap, :] = imgResize
 
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
@@ -50,5 +50,5 @@ while True:
     key = cv2.waitKey(1)
     if key == ord("s"):
         counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        cv2.imwrite(f"{folder}/Image_{time.time()}.jpg", imgWhite)
         print(counter)
